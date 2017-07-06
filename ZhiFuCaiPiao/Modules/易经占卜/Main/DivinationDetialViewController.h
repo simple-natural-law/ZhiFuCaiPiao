9af902effcc1ee0532979ba0005bdbf6428d27ae
zhangshijian <zsjcwy_xcode@163.com>
2017-07-06 22:24:42 +0800

    commit-a

---
+++ ZhiFuCaiPiao/Modules/易经占卜/Main/DivinationDetialViewController.h
@@ -0,0 +1,13 @@
+//
+//  DivinationDetialViewController.h
+//  ZhiFuCaiPiao
+//
+//  Created by 张诗健 on 2017/7/6.
+//  Copyright © 2017年 张诗健. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface DivinationDetialViewController : BaseViewController
+
+@end
--- ZhiFuCaiPiao/Modules/易经占卜/Main/DivinationMainViewController.m
+++ ZhiFuCaiPiao/Modules/易经占卜/Main/DivinationMainViewController.m
@@ -10,14 +10,52 @@
 
 @interface DivinationMainViewController ()
 
+@property (weak, nonatomic) IBOutlet UIButton *taijiButton;
+
+@property (strong, nonatomic) dispatch_source_t animationTimer;
+
 @end
+
 
 @implementation DivinationMainViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
 }
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+   
+    self.animationTimer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
+    dispatch_source_set_timer(self.animationTimer, DISPATCH_TIME_NOW, 0.05 * NSEC_PER_SEC, 0 * NSEC_PER_SEC);
+    dispatch_source_set_event_handler(self.animationTimer, ^{
+        
+        self.taijiButton.transform = CGAffineTransformRotate(self.taijiButton.transform, M_PI/180);
+    });
+    
+    dispatch_resume(self.animationTimer);
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    dispatch_source_cancel(self.animationTimer);
+    
+    self.animationTimer = nil;
+}
+
+
+- (IBAction)goDivination:(id)sender
+{
+    [self pushViewControllerKey:@"DivinationDetialViewController" param:nil animated:YES];
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
