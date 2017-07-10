8f1d36beb181d46d37d2a70dd0f77cdd1e5f9120
zhangshijian <zsjcwy_xcode@163.com>
2017-07-10 22:53:07 +0800

    commit

--- ZhiFuCaiPiao/Modules/易经占卜/Main/DivinationDescriptionViewController.m
+++ ZhiFuCaiPiao/Modules/易经占卜/Main/DivinationDescriptionViewController.m
@@ -11,7 +11,33 @@
 
 
 @interface DivinationDescriptionViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *numLabel1;
+@property (weak, nonatomic) IBOutlet UILabel *numLabel2;
+@property (weak, nonatomic) IBOutlet UILabel *numLabel3;
+@property (weak, nonatomic) IBOutlet UILabel *numLabel4;
+@property (weak, nonatomic) IBOutlet UILabel *numLabel5;
+@property (weak, nonatomic) IBOutlet UILabel *numLabel6;
+@property (weak, nonatomic) IBOutlet UILabel *numLabel7;
+@property (weak, nonatomic) IBOutlet UILabel *numLabel8;
 
+@property (weak, nonatomic) IBOutlet UILabel *tianGanLabel;
+@property (weak, nonatomic) IBOutlet UIView *tianView1;
+@property (weak, nonatomic) IBOutlet UIView *tianView2;
+@property (weak, nonatomic) IBOutlet UIView *tianView3;
+@property (weak, nonatomic) IBOutlet UIView *tianView4;
+@property (weak, nonatomic) IBOutlet UIView *tianView5;
+@property (weak, nonatomic) IBOutlet UIView *tianView6;
+
+@property (weak, nonatomic) IBOutlet UILabel *diZhiLabel;
+@property (weak, nonatomic) IBOutlet UIView *diView1;
+@property (weak, nonatomic) IBOutlet UIView *diView2;
+@property (weak, nonatomic) IBOutlet UIView *diView3;
+@property (weak, nonatomic) IBOutlet UIView *diView4;
+@property (weak, nonatomic) IBOutlet UIView *diView5;
+@property (weak, nonatomic) IBOutlet UIView *diView6;
+
+@property (weak, nonatomic) IBOutlet UILabel *guaJieLabel;
+@property (weak, nonatomic) IBOutlet UILabel *guaJieLabelHeightCons;
 @end
 
 @implementation DivinationDescriptionViewController
@@ -23,6 +49,23 @@
     self.title = @"周易预测-双色球";
     
     [self createAndSetRightButtonWithTitle:@"投注" touchUpInsideAction:@selector(touZhu)];
+    
+    self.tianGanLabel.text = self.param[@"GuaMing"][@"TianGan"];
+    self.diZhiLabel.text   = self.param[@"GuaMing"][@"DiZhi"];
+    
+    self.tianView1.hidden  = [self.param[@"GuaMing"][@"TianGanXiang"][0] integerValue] == 0;
+    self.tianView2.hidden  = [self.param[@"GuaMing"][@"TianGanXiang"][1] integerValue] == 0;
+    self.tianView3.hidden  = [self.param[@"GuaMing"][@"TianGanXiang"][2] integerValue] == 0;
+    self.tianView4.hidden  = [self.param[@"GuaMing"][@"TianGanXiang"][3] integerValue] == 0;
+    self.tianView5.hidden  = [self.param[@"GuaMing"][@"TianGanXiang"][4] integerValue] == 0;
+    self.tianView6.hidden  = [self.param[@"GuaMing"][@"TianGanXiang"][5] integerValue] == 0;
+    
+    self.diView1.hidden    = [self.param[@"GuaMing"][@"DiZhiXiang"][0] integerValue] == 0;
+    self.diView2.hidden    = [self.param[@"GuaMing"][@"DiZhiXiang"][1] integerValue] == 0;
+    self.diView3.hidden    = [self.param[@"GuaMing"][@"DiZhiXiang"][2] integerValue] == 0;
+    self.diView4.hidden    = [self.param[@"GuaMing"][@"DiZhiXiang"][3] integerValue] == 0;
+    self.diView5.hidden    = [self.param[@"GuaMing"][@"DiZhiXiang"][4] integerValue] == 0;
+    self.diView6.hidden    = [self.param[@"GuaMing"][@"DiZhiXiang"][5] integerValue] == 0;
 }
 
 
